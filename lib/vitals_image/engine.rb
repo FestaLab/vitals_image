93d800624738d95e39731864188ec5ec57d4c621
Lucas Camargo <lucas98martelli@hotmail.com>
2025-12-10 13:44:00 -0300

    go for safe solution

--- lib/vitals_image/engine.rb
+++ lib/vitals_image/engine.rb
@@ -23,6 +23,7 @@ require "vitals_image/optimizer/variable"
 require "vitals_image/optimizer/invariable"
 require "vitals_image/optimizer/unoptimizable"
 require "vitals_image/tiny_gif"
+require_relative "../../app/helpers/vitals_image/tag_helper"
 
 module VitalsImage
   class Engine < ::Rails::Engine
@@ -86,7 +87,7 @@ module VitalsImage
       end
     end
 
-    config.to_prepare do
+    initializer "vitals_image.action_controller" do
       ActiveSupport.on_load :action_controller do
         helper VitalsImage::TagHelper
       end
